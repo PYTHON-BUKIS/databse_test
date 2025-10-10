c6344d2b2552a7da989d58f1cb1904c5acd31cf8
Frances Bukis <123521028+PYTHON-BUKIS@users.noreply.github.com>
2025-10-10 22:34:38 +0800

    seeders

--- laravel-test/database/migrations/2025_10_10_091034_create_products_table.php
+++ laravel-test/database/migrations/2025_10_10_091034_create_products_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string("product_name", 30);
+            $table->string("product_description", 30);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
